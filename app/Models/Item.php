59d7086d6ffa4639ac66c55ffc131f42f6a65ffc
hassy-gh <shufc0107@gmail.com>
2022-04-25 16:59:04 +0900

    Initialized a new repository.

---
+++ app/Models/Item.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    // 出品中
+    const STATE_SELLING = 'selling';
+    // 購入済み
+    const STATE_BOUGHT = 'bought';
+}
